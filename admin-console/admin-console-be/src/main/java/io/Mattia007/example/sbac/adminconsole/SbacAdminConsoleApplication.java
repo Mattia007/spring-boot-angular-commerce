26ad95dcd7f28eb9ed80f37358ab90ada2bfdf8c
Mattia007 <mattia.ortolani@proton.me>
2023-11-16 23:01:51 +0100

    pom multiproject spring boot module

---
+++ admin-console/admin-console-be/src/main/java/io/Mattia007/example/sbac/adminconsole/SbacAdminConsoleApplication.java
@@ -0,0 +1,13 @@
+package io.Mattia007.example.sbac.adminconsole;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbacAdminConsoleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbacAdminConsoleApplication.class, args);
+	}
+
+}
---
+++ admin-console/admin-console-be/src/test/java/io/Mattia007/example/sbac/adminconsole/SbacAdminConsoleApplicationTests.java
@@ -0,0 +1,13 @@
+package io.Mattia007.example.sbac.adminconsole;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbacAdminConsoleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
